8ec252b0b6fdef4ac2834d9e241b9661db122b52
hishidama <hishi.dama@asahi.email.ne.jp>
2020-12-08 13:14:13 +0900

    Added bb_measurement_value.sql

---
+++ plsql/bb_measurement_value.body.sql
@@ -0,0 +1,88 @@
+create or replace package body bb_measurement_value
+is
+
+  function create_value(unit varchar2, value number) return bb_measurement_value
+  is
+    v bb_measurement_value;
+  begin
+    v.unit  := unit;
+    v.value := value;
+    return v;
+  end;
+
+  function add(
+    value1 bb_measurement_value,
+    value2 bb_measurement_value
+  ) return bb_measurement_value
+  is
+    common_unit measurement_master.m_unit%type;
+    v1 number;
+    v2 number;
+  begin
+    if value1.value is null then
+      return value2;
+    end if;
+    if value1.unit = value2.unit then
+      return create_value(value1.unit, value1.value + value2.value);
+    end if;
+
+    common_unit := bb_measurement.get_common_unit(value1.unit, value2.unit);
+    v1 := bb_measurement.convert_unit(value1.value, value1.unit, common_unit);
+    v2 := bb_measurement.convert_unit(value2.value, value2.unit, common_unit);
+    return create_value(common_unit, v1 + v2);
+  end;
+
+  function multiply(
+    value1 bb_measurement_value,
+    value2 number
+  ) return bb_measurement_value
+  is
+  begin
+    return create_value(value1.unit, value1.value * value2);
+  end;
+
+  function divide(
+    value1 bb_measurement_value,
+    value2 bb_measurement_value
+  ) return number
+  is
+    common_unit measurement_master.m_unit%type;
+    v1 number;
+    v2 number;
+  begin
+    common_unit := bb_measurement.get_common_unit(value1.unit, value2.unit);
+    v1 := bb_measurement.convert_unit(value1.value, value1.unit, common_unit);
+    v2 := bb_measurement.convert_unit(value2.value, value2.unit, common_unit);
+    return v1 / v2;
+  end;
+
+  function convert_unit(
+    value    bb_measurement_value,
+    dst_unit varchar2
+  ) return bb_measurement_value
+  is
+  begin
+    if value.unit = dst_unit then
+      return value;
+    end if;
+    return create_value(
+      dst_unit,
+      bb_measurement.convert_unit(value.value, value.unit, dst_unit)
+    );
+  end;
+
+  function get_common_unit_value(
+    value1 bb_measurement_value,
+    value2 bb_measurement_value
+  ) return bb_value_pair
+  is
+    result bb_value_pair;
+  begin
+    result.unit := bb_measurement.get_common_unit(value1.unit, value2.unit);
+    result.value1 := bb_measurement.convert_unit(value1.value, value1.unit, result.unit);
+    result.value2 := bb_measurement.convert_unit(value2.value, value2.unit, result.unit);
+    return result;
+  end;
+
+end;
+/
---
+++ plsql/bb_measurement_value.sql
@@ -0,0 +1,43 @@
+create or replace package bb_measurement_value
+is
+
+  type bb_measurement_value is record (
+    value number,
+    unit measurement_master.m_unit%type
+  );
+
+  function create_value(unit varchar2, value number) return bb_measurement_value;
+
+  function add(
+    value1 bb_measurement_value,
+    value2 bb_measurement_value
+  ) return bb_measurement_value;
+
+  function multiply(
+    value1 bb_measurement_value,
+    value2 number
+  ) return bb_measurement_value;
+
+  function divide(
+    value1 bb_measurement_value,
+    value2 bb_measurement_value
+  ) return number;
+
+  function convert_unit(
+    value    bb_measurement_value,
+    dst_unit varchar2
+  ) return bb_measurement_value;
+
+  type bb_value_pair is record (
+    unit measurement_master.m_unit%type,
+    value1 number,
+    value2 number
+  );
+
+  function get_common_unit_value(
+    value1 bb_measurement_value,
+    value2 bb_measurement_value
+  ) return bb_value_pair;
+
+end;
+/
