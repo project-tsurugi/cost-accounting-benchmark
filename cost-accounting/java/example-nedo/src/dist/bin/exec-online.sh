ea404921d4d483f8fefac962814aa8459d4654ab
Hishida Masato <hishida@nautilus-technologies.com>
2021-03-05 10:54:59 +0900

    BenchOnline: mainの引数で日付や工場IDを指定する

---
+++ cost-accounting/java/example-nedo/src/dist/bin/exec-online.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+BASEDIR=$(cd $(dirname $0)/..; pwd)
+
+PROPERTY=$1
+if [ -z "$PROPERTY" ]
+then
+  echo usage $0 property-file
+  exit 1
+fi
+
+function call_java() {
+  echo $1
+  java -cp "$BASEDIR/*:$BASEDIR/lib/*" -Dproperty="$PROPERTY" $@
+}
+
+call_java com.example.nedo.online.BenchOnline 2020-09-15 "1-2,3-4,5-6"
+# ex) 1,2-4
+# -> thead1: factoryId=1; thread2=factoryId=2,3,4
--- cost-accounting/java/example-nedo/src/main/java/com/example/nedo/online/BenchOnline.java
+++ cost-accounting/java/example-nedo/src/main/java/com/example/nedo/online/BenchOnline.java
@@ -3,7 +3,9 @@ package com.example.nedo.online;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
@@ -13,16 +15,53 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.seasar.doma.jdbc.tx.TransactionManager;
+
 import com.example.nedo.init.InitialData;
+import com.example.nedo.jdbc.doma2.config.AppConfig;
+import com.example.nedo.jdbc.doma2.dao.FactoryMasterDao;
+import com.example.nedo.jdbc.doma2.dao.FactoryMasterDaoImpl;
 
 public class BenchOnline {
 
 	public static void main(String[] args) {
+		LocalDate batchDate = InitialData.DEFAULT_BATCH_DATE;
+		if (args.length >= 1) {
+			batchDate = LocalDate.parse(args[0]);
+		}
+
 		List<BenchOnlineThread> threadList = new ArrayList<>();
-		// TODO argsから工場IDと日付を取得
-		create1(threadList, 1, 2, InitialData.DEFAULT_BATCH_DATE);
-		create1(threadList, 3, 4, InitialData.DEFAULT_BATCH_DATE);
-		create1(threadList, 5, 6, InitialData.DEFAULT_BATCH_DATE);
+		if (args.length >= 2) {
+			if (!args[1].trim().equalsIgnoreCase("all")) {
+				Set<Integer> set = new HashSet<>();
+				String[] ss = args[1].split(",");
+				for (String s : ss) {
+					int n = s.indexOf('-');
+					if (n >= 0) {
+						int start = Integer.parseInt(s.substring(0, n).trim());
+						int end = Integer.parseInt(s.substring(n + 1).trim());
+						for (int id = start; id <= end; id++) {
+							if (!set.add(id)) {
+								throw new IllegalArgumentException("duplicate id=" + id);
+							}
+						}
+						create1(threadList, start, end, batchDate);
+					} else {
+						int id = Integer.parseInt(s.trim());
+						if (!set.add(id)) {
+							throw new IllegalArgumentException("duplicate id=" + id);
+						}
+						create1(threadList, id, id, batchDate);
+					}
+				}
+			}
+		}
+		if (threadList.isEmpty()) {
+			List<Integer> factoryList = getAllFactory();
+			int start = factoryList.stream().mapToInt(n -> n).min().getAsInt();
+			int end = factoryList.stream().mapToInt(n -> n).max().getAsInt();
+			create1(threadList, start, end, batchDate);
+		}
 
 		ExecutorService pool = newExecutorService(threadList.size());
 
@@ -47,7 +86,18 @@ public class BenchOnline {
 		// TODO 処理件数カウンター（バッチの実行中のものだけカウントする、なんて出来るのか？）
 	}
 
+	private static List<Integer> getAllFactory() {
+		FactoryMasterDao dao = new FactoryMasterDaoImpl();
+
+		TransactionManager tm = AppConfig.singleton().getTransactionManager();
+
+		return tm.required(() -> {
+			return dao.selectAllId();
+		});
+	}
+
 	private static void create1(List<BenchOnlineThread> threadList, int start, int end, LocalDate date) {
+		System.out.printf("create thread: factoryId=%d-%d%n", start, end);
 		List<Integer> factoryList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
 		BenchOnlineThread thread = new BenchOnlineThread(factoryList, date);
 		threadList.add(thread);
