0e3bb473c932fba23b63cdd41f30d5250207865d
Hishida Masato <hishida@nautilus-technologies.com>
2023-02-21 11:16:44 +0900

    Added cb.sh

---
+++ java/cost-accounting-benchmark/src/dist/bin/cb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+BASEDIR=$(cd $(dirname $0)/..; pwd)
+
+PROPERTY=$1
+if [ -z "$PROPERTY" ]
+then
+  echo usage $0 property-file
+  exit 1
+fi
+
+function call_java() {
+  java -cp "$BASEDIR/*:$BASEDIR/lib/*" $JAVA_OPTS -Dproperty="$PROPERTY" $RUN_JAVA_OPTS $@
+}
+
+# init data
+call_java com.tsurugidb.benchmark.costaccounting.init.InitialData
+
+# batch-command
+call_java com.tsurugidb.benchmark.costaccounting.Main executeBatch
