311c396b7d0f24fa73aafac5223c1e8dde396473
Hishida Masato <hishida@nautilus-technologies.com>
2023-03-30 17:24:29 +0900

    TateyamaWatcher: Added thread name

--- java/cost-accounting-benchmark/src/main/java/com/tsurugidb/benchmark/costaccounting/watcher/TateyamaWatcher.java
+++ java/cost-accounting-benchmark/src/main/java/com/tsurugidb/benchmark/costaccounting/watcher/TateyamaWatcher.java
@@ -28,6 +28,7 @@ public class TateyamaWatcher implements Runnable {
 
     @Override
     public void run() {
+        Thread.currentThread().setName("TateyamaWatcher");
         final NumberFormat fmt = NumberFormat.getNumberInstance(Locale.US);
 
         if (pid == -1) {
