39a7012f7fa3ff7888ad3ee6ad642493299f7ed2
Hishida Masato <hishida@nautilus-technologies.com>
2023-06-20 14:58:07 +0900

    rename TateyamaWatcherTest to TsurugidbWatcherTest

--- java/cost-accounting-benchmark/src/test/java/com/tsurugidb/benchmark/costaccounting/watcher/TsurugidbWatcherTest.java
+++ java/cost-accounting-benchmark/src/test/java/com/tsurugidb/benchmark/costaccounting/watcher/TsurugidbWatcherTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class TateyamaWatcherTest {
+class TsurugidbWatcherTest {
 
     @Test
     void testParseMemoryValue() {
